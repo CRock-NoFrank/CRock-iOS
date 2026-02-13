ea08c286ceea42746ac1550f711539ae180b4f47
JUDY LEE <ljh.social@ewhain.net>
2026-02-13 19:34:28 +0900

    feat: 요일 순서 변경

--- T8-NoFrank/Sources/Models/Weekday.swift
+++ T8-NoFrank/Sources/Models/Weekday.swift
@@ -16,7 +16,7 @@ enum Weekday: Int, CaseIterable {
     case fri = 6
     case sat = 7
 
-    static let ordered: [Weekday] = [.mon, .tue, .wed, .thu, .fri, .sat, .sun]
+    static let ordered: [Weekday] = [.sun, .mon, .tue, .wed, .thu, .fri, .sat]
     static let weekdayCount = 7
 
     var labelKey: String {
